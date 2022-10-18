bb4a46dc1c7c46197cef8e559596410ebdcb365a
bburton5 <bburton5@uw.edu>
2022-10-17 18:52:53 -0700

    modified seeds

--- seeds/missingPetData.js
+++ seeds/missingPetData.js
@@ -1,5 +1,19 @@
 const { MissingPet } = require("../models");
 
-const missingpetdata = [];
+const missingpetdata = [
+  {
+    pet_name: "Nyla",
+    pet_type: "Cat",
+    description:
+      " 10 year old Abyssinian/tabby mix. She has a ticked coat on body, strips on legs and tail. With 'necklace' stripe. Red collar. Chipped",
+    image:
+      "https://petharbor.com/get_image.asp?RES=Detail&LOCATION=PUBLIC&ID=2501953",
+    zip_code: "98188",
+    date_lost: "2022-09-14",
+    contact: "1234567890",
+  },
+];
 
-const seedMissingPet = () => MissingPet.bulkCreate;
+const seedMissingPet = () => MissingPet.bulkCreate(missingpetdata);
+
+module.exports = seedMissingPet;
