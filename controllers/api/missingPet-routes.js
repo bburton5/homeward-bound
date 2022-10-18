8d062782291c50c9f4cd4ba5322b90e80e50b029
SpencerNelsonUW <spencernelsonn@gmail.com>
2022-10-18 16:16:17 -0700

    fixed post route

--- controllers/api/missingPet-routes.js
+++ controllers/api/missingPet-routes.js
@@ -1,27 +1,25 @@
 const router = require('express').Router();
-const { MissingPet } = require('../../models/MissingPet');
+const { MissingPet } = require('../../models');
 
-router.post('/newpost', async (req, res) => {
+router.post('/', async (req, res) => {
   try {
     const missingPetData = await MissingPet.create ({
       pet_name: req.body.pet_name,
       pet_type: req.body.pet_type,
       description: req.body.description,
-      image: req.body.image,
       zip_code: req.body.zip_code,
       date_lost: req.body.date_lost,
       contact_number: req.body.contact_number,
-    });
-    console.log(missingPetData,"in the try");
+    })
+    res.status(200).json(missingPetData)
   } catch (err){
     res.status(500).json(err);
   }
-  console.log(missingPetData,"out the try");
 }); 
 
 
 //GET all MissingPet
-router.get('/newpost', async (req, res) => {
+router.get('/', async (req, res) => {
   try {
   // Search the database for all 
   const allMissingPets = await MissingPet.findAll()
--- models/MissingPet.js
+++ models/MissingPet.js
@@ -7,33 +7,38 @@ MissingPet.init(
   {
     id: {
       type: DataTypes.INTEGER,
-      allowNull: false,
+      allowNull: true,
       primaryKey: true,
       autoIncrement: true,
     },
     pet_name: {
       type: DataTypes.STRING,
-      allowNull: false,
+      allowNull: true,
     },
     pet_type: {
       type: DataTypes.STRING,
-      allowNull: false,
+      allowNull: true,
+      defaultValue:"animal",
     },
     description: {
       type: DataTypes.STRING,
-      allowNull: false,
+      allowNull: true,
+      defaultValue:"animal",
     },
     zip_code: {
       type: DataTypes.STRING,
-      allowNull: false,
+      allowNull: true,
+      defaultValue:"123123",
     },
     date_lost: {
       type: DataTypes.STRING,
       allowNull: true,
+      defaultValue:"2022-10-18",
     },
     contact_number: {
       type: DataTypes.STRING,
       allowNull: true,
+      defaultValue:"11111",
     },
   },
   {
