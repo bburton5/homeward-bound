f365b2393c763d9fc4ba4cbdc205d7b8746f9fe1
rong xu <rongxu@MacBook-Pro-3.local>
2022-10-18 23:57:18 -0700

    update seed.js & adding README.md

--- controllers/seed.js
+++ controllers/seed.js
@@ -146,19 +146,6 @@ router.post('/seed', (req, res) => {
       description: 'slender tall very short hair. She has a harness and leash and was left at Dumas bay with some food in a wire crate (Aspen brand ).',
     },
 
-    {//12
-      animal_name: '',
-      animal_type: '',
-      animal_age: A ,
-      animal_gender: A,
-      animal_breed: A,
-      animal_color: A,
-      found_on: A,
-      found_at: A,
-      image: '',
-      description: A,
-    },
-
     ])
 
      MissingPet.bulkCreate([
@@ -363,6 +350,166 @@ router.post('/seed', (req, res) => {
       },
 
       {//21
+        pet_name: Nino,
+        pet_type: Cat,
+        description: 'Black domestic short hair, male, neutered, yellow eyes, missing a front bottom K9 tooth, has a few white hairs on his chest. He was wearing a neon green reflective collar with some tags on it. He is an indoor/outdoor cat but not from the area he was lost. Responds to being called baby. He is about 6 years old.',
+        image: 'https://petharbor.com/PublicDetail.asp?searchtype=PUBFOUND&view=sysadm.v_animal&shelterlist=%27KING%27&where=type&ID=2501475',
+        zip_code: none,
+        date_lost: 09/19/2022,
+        contact_number: none,
+      },
+
+      {//22
+        pet_name: Jack,
+        pet_type: Cat,
+        description: 'Black face, black ears , black legs and paws, long black bushy tail, Cream, tan and gray on his body, Big Bright Blue eyes, Jack is shy',
+        image: 'https://petharbor.com/PublicDetail.asp?searchtype=PUBFOUND&view=sysadm.v_animal&shelterlist=%27KING%27&where=type&ID=2504248',
+        zip_code: none,
+        date_lost: 09/25/2022,
+        contact_number: none,
+      },
+
+      {//23
+        pet_name: Luna,
+        pet_type: Cat,
+        description: 'Black female cat, 4.5 years old and about 16 pounds. She is spayed and microchipped. She has medium/short hair and is all black except for a white spot on her chest and a white "bikini" on her belly.',
+        image: 'https://petharbor.com/PublicDetail.asp?searchtype=PUBFOUND&view=sysadm.v_animal&shelterlist=%27KING%27&where=type&ID=2503021',
+        zip_code: 98168,
+        date_lost: 09/29/2022,
+        contact_number: none,
+      },
+
+      {//24
+        pet_name: Zoey,
+        pet_type: Dog,
+        description: 'Black German Shepard with a white patch on her chest she won’t have a collar on it was left home.        ',
+        image: none,
+        zip_code: none,
+        date_lost: 10/18/2022,
+        contact_number: none,
+      },
+
+      {//25
+        pet_name: Poppy,
+        pet_type: Cat,
+        description: 'Black, grey, and orange tabby. She has an orange spot on her head. Wears a pink collar.',
+        image: 'https://petharbor.com/PublicDetail.asp?searchtype=PUBFOUND&view=sysadm.v_animal&shelterlist=%27KING%27&where=type&ID=2506284',
+        zip_code: none,
+        date_lost: 09/129/2022,
+        contact_number: none,
+      },
+
+      {//26
+        pet_name: Jaxson,
+        pet_type: Cat,
+        description: 'Black male cat. 6.5 years old.',
+        image: none,
+        zip_code: 98030,
+        date_lost: 10/17/2022,
+        contact_number: none,
+      },
+
+      {//27
+        pet_name: Peepers,
+        pet_type: Dog,
+        description: 'Blue eyes, black tri color.',
+        image: 'https://petharbor.com/PublicDetail.asp?searchtype=PUBFOUND&view=sysadm.v_animal&shelterlist=%27KING%27&where=type&ID=2497311',
+        zip_code: 98188,
+        date_lost: 09/14/2022,
+        contact_number: none,
+      },
+
+      {//28
+        pet_name: Blue,
+        pet_type: Dog,
+        description: 'Blue is a blue heeler, 50-60lbs, green collar, not microchipped but responds to his name, very friendly - last scene at or around Kent Public Library. He is approximately 2 years old. White with black spots, looks like a dalmatian.',
+        image: none,
+        zip_code: 98188,
+        date_lost: 09/11/2022,
+        contact_number: none,
+      },
+
+      {//29
+        pet_name: Bolita,
+        pet_type: Dog,
+        description: 'Bolita is a toy poodle white coat , about 15lbs. She’s easily frighten by people. Friendly and playful.',
+        image: 'https://petharbor.com/PublicDetail.asp?searchtype=PUBFOUND&view=sysadm.v_animal&shelterlist=%27KING%27&where=type&ID=2509984',
+        zip_code: 981002,
+        date_lost: 10/12/2022,
+        contact_number: none,
+      },
+
+      {//30
+        pet_name: Duke,
+        pet_type: Dog,
+        description: 'Brindle Blue nose pit, German shepherd, Akita mix. Male-neutered, 5yrs old. ',
+        image: 'https://petharbor.com/PublicDetail.asp?searchtype=PUBFOUND&view=sysadm.v_animal&shelterlist=%27KING%27&where=type&ID=2501391',
+        zip_code: none,
+        date_lost: 09/26/2022,
+        contact_number: none,
+      },
+
+      {//31
+        pet_name: Nora,
+        pet_type: Dog,
+        description: 'Brown 10 pounds, shy and fast.',
+        image: none,
+        zip_code: 98019,
+        date_lost: 10/09/2022,
+        contact_number: none,
+      },
+
+      {//32
+        pet_name: Penelope,
+        pet_type: Dog,
+        description: 'Brown and black Chihuahua/miniature pinscher mix. Around 9 pounds. Very anxious. Some white hairs on her back.',
+        image: none,
+        zip_code: 98042,
+        date_lost: 09/22/2022,
+        contact_number: none,
+      },
+
+      {//33
+        pet_name: Brandon,
+        pet_type: Cat,
+        description: 'Brown and gray striped tabby with white paws.',
+        image: 'https://petharbor.com/PublicDetail.asp?searchtype=PUBFOUND&view=sysadm.v_animal&shelterlist=%27KING%27&where=type&ID=2507069',
+        zip_code: 98005,
+        date_lost: 10/04/2022,
+        contact_number: none,
+      },
+
+      {//34
+        pet_name: Groot,
+        pet_type: Dog,
+        description: 'Brown small.',
+        image: 'https://petharbor.com/PublicDetail.asp?searchtype=PUBFOUND&view=sysadm.v_animal&shelterlist=%27KING%27&where=type&ID=2510521',
+        zip_code: 98188,
+        date_lost: 10/15/2022,
+        contact_number: none,
+      },
+
+      {//35
+        pet_name: Unknown,
+        pet_type: Cat,
+        description: 'Brown tabby cat. Shes pretty shy but is easy to get along with',        
+        image: 'https://petharbor.com/PublicDetail.asp?searchtype=PUBFOUND&view=sysadm.v_animal&shelterlist=%27KING%27&where=type&ID=2507313',
+        zip_code: none,
+        date_lost: 10/09/2022,
+        contact_number: none,
+      },
+
+      {//36
+        pet_name: Sybill,
+        pet_type: Cat,
+        description: 'Brown tabby with white chest and mostly white belly, white paws with more white on the hind legs. Patch of belly is currently shaved.',
+        image: 'https://petharbor.com/PublicDetail.asp?searchtype=PUBFOUND&view=sysadm.v_animal&shelterlist=%27KING%27&where=type&ID=2500628',
+        zip_code: 98030,
+        date_lost: 09/23/2022,
+        contact_number: none,
+      },
+
+      {//37
         pet_name: '',
         pet_type: '',
         description: '.',
@@ -372,6 +519,74 @@ router.post('/seed', (req, res) => {
         contact_number: none,
       },
 
+      {//38
+        pet_name: Lucy,
+        pet_type: Cat,
+        description: 'Brown with stripes',
+        image: 'https://petharbor.com/PublicDetail.asp?searchtype=PUBFOUND&view=sysadm.v_animal&shelterlist=%27KING%27&where=type&ID=2501624',
+        zip_code: 98040,
+        date_lost: 09/24/2022,
+        contact_number: none,
+      },
 
+      {//39
+        pet_name: Cascade,
+        pet_type: Dog,
+        description: 'Cascade is a husky mix. Black and white long coat wearing a green collar, no tags.',
+        image: 'https://petharbor.com/PublicDetail.asp?searchtype=PUBFOUND&view=sysadm.v_animal&shelterlist=%27KING%27&where=type&ID=2507985',
+        zip_code: 98188,
+        date_lost: 10/11/2022,
+        contact_number: none,
+      },
+
+      {//40
+        pet_name: Aspen,
+        pet_type: Cat,
+        description: 'Charcoal gray cat with yellow eyes and blue collar with a bell.',
+        image: 'https://petharbor.com/PublicDetail.asp?searchtype=PUBFOUND&view=sysadm.v_animal&shelterlist=%27KING%27&where=type&ID=2505814',
+        zip_code: none,
+        date_lost: 10/06/2022,
+        contact_number: none,
+      },
+
+      {//41
+        pet_name: Nacho,
+        pet_type: Dog,
+        description: 'Chihuahua, Corgie, Pomeranian Mix.',
+        image: 'https://petharbor.com/PublicDetail.asp?searchtype=PUBFOUND&view=sysadm.v_animal&shelterlist=%27KING%27&where=type&ID=2510103',
+        zip_code: none,
+        date_lost: 09/18/2022,
+        contact_number: none,
+      },
+
+      {//42
+        pet_name: Lexi,
+        pet_type: Dog,
+        description: 'Chocolate Australian labradoodle. 25 lbs with some white streaks. Chipped with blue scar on belly. Neutered',
+        image: 'https://petharbor.com/PublicDetail.asp?searchtype=PUBFOUND&view=sysadm.v_animal&shelterlist=%27KING%27&where=type&ID=2501617',
+        zip_code: none,
+        date_lost: 09/26/2022,
+        contact_number: none,
+      },
+
+      {//43
+        pet_name: Chorles,
+        pet_type: Cat,
+        description: 'Chorles is a small 1 year old spayed female Bombay with shiny black short fur with a tiny bit of white on the tummy.',
+        image: 'https://petharbor.com/PublicDetail.asp?searchtype=PUBFOUND&view=sysadm.v_animal&shelterlist=%27KING%27&where=type&ID=2504993',
+        zip_code: 98042,
+        date_lost: 09/24/2022,
+        contact_number: none,
+      },
+
+      {//44
+        pet_name: Happy,
+        pet_type: Cat,
+        description: 'Chubby gray and white tuxedo. White cheeks, right tipped ear from spay. Two dark spots on right side of pink/white nose. All white boots, white bib and belly.',
+        image: 'https://petharbor.com/PublicDetail.asp?searchtype=PUBFOUND&view=sysadm.v_animal&shelterlist=%27KING%27&where=type&ID=2506862',
+        zip_code: 98168,
+        date_lost: 10/08/2022,
+        contact_number: none,
+      },
         ])
     })
